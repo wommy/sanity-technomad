ca69e3b1f4083b86b59b011e2b8698a951ee8179
wommy <tom@technomad.media>
2021-08-31 11:25:30 -0400

    feat: todo ref array

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -5,6 +5,7 @@ export const todo = {
     { type:'string', name:'todo' },
     { type:'array', name:'category', of:[{ type:'string' }], options:{ layout:'tags' }, initialValue:['inbox'] },
     { type:'boolean', name:'isComplete', initialValue:false },
+    { type:'array', name:'refs', of:[{ type:'reference', name:'bookmark', to:[{ type:'bookmark' }] }], options:{ editModal:'popover' } }
   ], 
 }
 
